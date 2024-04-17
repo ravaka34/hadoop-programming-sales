49dca9a45ec5f6d9bb9c549cf22790ca4120302f
ravaka34 <ravakanyaina34@gmail.com>
2024-04-17 10:21:35 +0300

    Init Maven Project

---
+++ src/test/java/org/hadoop/sales/AppTest.java
@@ -0,0 +1,18 @@
+package org.hadoop.sales;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
